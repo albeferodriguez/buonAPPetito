d8f4313b74586b36666f7e405a3c4b57534cd51f
Alberto Fernández-Baíllo <alberto.fernandezbaillo@mo2o.com>
2022-11-02 19:14:27 +0100

    initial commit

---
+++ buonAPPetito/buonAPPetito/buonAPPetitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  buonAPPetitoApp.swift
+//  buonAPPetito
+//
+//  Created by Alberto Fernandez-Baillo on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct buonAPPetitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
